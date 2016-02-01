8618a059bc2ed03882556c88f41f82cd8b01a201
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-02-01 14:08:03 +0000

    commented code removed, scripts added

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 """A setup up script for the APEL-SSM."""
 from setuptools import setup, find_packages
 
-# required to parse the requiremnts.txt file
-# from pip.req import parse_requirements
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-# install_requirements = parse_requirements("requirements.txt")
-# print str(install_reqs).strip('[]')
-
-# nned install_requirements as a list of requirements
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-# install_requirements = [str(ir.req) for ir in install_reqs]
 
 setup(name='ssm',
       version='2.1.7-1',
@@ -28,4 +18,5 @@ setup(name='ssm',
           'coveralls': ['coveralls'],
       },
       packages=find_packages('ssm'),  # include all packages under ssm
-      package_dir={'': 'ssm'})  # tell distutils packages are under ssm
+      package_dir={'': 'ssm'},  # tell distutils packages are under ssm
+      scripts=["bin/apel-ssm", "bin/receiver.py", "bin/sender.py"])
