d17de98efc68be934e8b8072c36d1dcd4e9df9eb
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-02-01 15:29:25 +0000

    change of name and url

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ def main():
     bin_dir = os.path.join(sys.prefix, "bin")
     bin_files = ['bin/apel-ssm', 'bin/receiver.py', 'bin/sender.py']
 
-    setup(name='ssm',
+    setup(name='apel-ssm',
           version='2.1.7-1',
           description=("Secure Stomp Messenger (SSM) is designed to simply "
                        "send messages using the STOMP protocol."),
           author='APEL',
           author_email='apel-admins@stfc.ac.uk',
-          url='https://github.com/apel/ssm',
+          url='https://wiki.egi.eu/wiki/APEL/SSM',
           download_url='https://github.com/apel/ssm/archive/2.1.7-1.zip',
           license='Apache License, v2 - http://www.apache.org/licenses/',
           install_requires=['stomp.py<=3.1.6', 'python-ldap', 'dirq'],
