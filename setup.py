8a61c71c69e2a394050970a3ef76ff294c3052d3
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-02-01 10:22:59 +0000

    syntax errors fixed

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ from setuptools import setup, find_packages
 setup(name='ssm',
       version='2.1.7-1',
       description=("Secure Stomp Messenger (SSM) is designed to simply send "
-                   "messages using the STOMP protocol.")
+                   "messages using the STOMP protocol."),
       author='APEL',
       author_email='apel-admins@stfc.ac.uk',
       url='https://github.com/apel/ssm',
       download_url='https://github.com/apel/ssm/archive/2.1.7-1.zip',
       license='Apache License, version 2 - http://www.apache.org/licenses/',
-      install_requires=['stomp.py<=3.1.6', 'python-ldap', 'dirq,']
+      install_requires=['stomp.py<=3.1.6', 'python-ldap', 'dirq'],
       extras_require={
           'python-daemon': ['python-daemon'],
           'unittest2': ['unittest2'],
