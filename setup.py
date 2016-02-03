f277b729b90f018c0775f7844c2c469af0e03a9a
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-02-03 10:02:25 +0000

    sender.py/receiver.py now installed as ssmsend and ssmreceive

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 """A setup up script for the APEL-SSM."""
+from shutil import copyfile
+from os import remove
 from setuptools import setup, find_packages
 
 
@@ -22,14 +24,28 @@ def main():
           install_requires=['stomp.py<=3.1.6', 'python-ldap', 'dirq'],
           extras_require={
               'python-daemon': ['python-daemon'],
-              'unittest2': ['unittest2'],
-              'coveralls': ['coveralls'],
           },
           packages=find_packages(exclude=['bin']),
-          scripts=['bin/apel-ssm', 'bin/receiver.py', 'bin/sender.py'],
+          scripts=['bin/apel-ssm', 'bin/ssmreceive', 'bin/ssmsend'],
           data_files=[(conf_dir, conf_files),
                       ('/etc/logrotate.d', ['conf/ssm.logrotate'])],
-          zip_safe=True)
+          zip_safe=True,
+          test_suite='test',
+          tests_require=['mock', 'unittest2', 'coveralls'])
+
+
+def rename_files():
+    """(Copy and) Rename receiver.py to ssmreceive and sender.py to ssmsend."""
+    copyfile('bin/receiver.py', 'bin/ssmreceive')
+    copyfile('bin/sender.py', 'bin/ssmsend')
+
+
+def delete_files():
+    """Delete the files bin/ssmreceive and bin/ssmsend."""
+    remove('bin/ssmreceive')
+    remove('bin/ssmsend')
 
 if __name__ == "__main__":
+    rename_files()
     main()
+    delete_files()
