961deaa8cef9759645b2872be60fcc9b8daea872
gregcorbett <greg.corbett@stfc.ac.uk>
2016-04-28 10:57:05 +0100

    Change test queue to /tmp/apel/

--- test/test_ssm.py
+++ test/test_ssm.py
@@ -94,7 +94,7 @@ class TestSsm(unittest.TestCase):
         # set SSM to use REST
         self._ssm._protocol = "REST"
         # set up a queue
-        self._ssm._outq = QueueSimple('/var/spool/apel/outgoing')
+        self._ssm._outq = QueueSimple('/tmp/apel/')
         # mock the send message
         self._ssm._send_msg_rest = MagicMock()
 
