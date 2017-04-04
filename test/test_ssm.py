81b8cbe095216fa67c305a677723507bba9477ea
Greg Corbett <greg.corbett@stfc.ac.uk>
2017-04-04 09:15:05 +0100

    Change test/test_ssm to use generated key/crt
    - the hardcoded certifcate needed changing anyway, as it had
      expired and we now check for that and raise an exception
      if the cert has expired.

--- test/test_ssm.py
+++ test/test_ssm.py
@@ -11,6 +11,7 @@ import unittest
 import shutil
 import tempfile
 
+from OpenSSL import crypto
 
 
 class TestSsm(unittest.TestCase):
@@ -20,31 +21,38 @@ class TestSsm(unittest.TestCase):
     
     def setUp(self):
         '''
-        Set up a test SSM, and a test directory containing certificates.
+        Set up a test SSM, and a test directory, and a key/cert pair.
         '''
+        if not (os.path.exists(TEST_CERT_FILE) and os.path.exists(TEST_KEY_FILE)):
+            # create a key pair
+            key_pair = crypto.PKey()
+            key_pair.generate_key(crypto.TYPE_RSA, 1024)
+            open(TEST_KEY_FILE, "w").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key_pair))
+
+            # create a self-signed cert
+            cert = crypto.X509()
+            cert.get_subject().C = "UK"
+            cert.get_subject().O = "STFC"
+            cert.get_subject().OU = "SC"
+            cert.get_subject().CN = "Test Cert"
+            cert.set_serial_number(1000)
+            cert.gmtime_adj_notBefore(0)
+            cert.gmtime_adj_notAfter(10*365*24*60*60)
+            cert.set_issuer(cert.get_subject())
+            cert.set_pubkey(key_pair)
+            cert.sign(key_pair, 'sha1')
+
+            open(TEST_CERT_FILE, "w").write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+
         self._tmp_dir = tempfile.mkdtemp(prefix='ssm')
-        
-        self.cert_file, self.cert_path = tempfile.mkstemp(prefix='cert', dir=self._tmp_dir)
-        os.write(self.cert_file, TEST_CERT)
-        os.close(self.cert_file)
-        
-        
-        self.key_file, self.key_path = tempfile.mkstemp(prefix='key', dir=self._tmp_dir)
-        os.write(self.key_file, TEST_KEY)
-        os.close(self.key_file)
         
         self._valid_dn = '/test/dn'
         self.valid_dn_file, self.valid_dn_path = tempfile.mkstemp(prefix='valid', dir=self._tmp_dir)
         os.write(self.valid_dn_file, self._valid_dn)
         os.close(self.valid_dn_file)
         
-        self._cert_string = TEST_CERT
-        self._key = TEST_KEY
-        
         hosts_and_ports = [('not.a.broker', 123)]
         capath = '/not/a/path'
-        certificate = self.cert_path
-        key = self.key_path
         check_crls = False
         pidfile = self._tmp_dir + '/pidfile'
         
@@ -54,7 +62,7 @@ class TestSsm(unittest.TestCase):
         
         self._msgdir =  tempfile.mkdtemp(prefix='msgq')
         
-        self._ssm = ssm.ssm2.Ssm2(hosts_and_ports, self._msgdir, certificate, key,
+        self._ssm = ssm.ssm2.Ssm2(hosts_and_ports, self._msgdir, TEST_CERT_FILE, TEST_KEY_FILE,
                                   dest=dest, listen=listen)
 
 
@@ -63,6 +71,8 @@ class TestSsm(unittest.TestCase):
         try:
             shutil.rmtree(self._msgdir)
             shutil.rmtree(self._tmp_dir)
+            os.remove(TEST_CERT_FILE)
+            os.remove(TEST_KEY_FILE)
         except OSError, e:
             print 'Error removing temporary directory %s' % self._tmp_dir
             print e
@@ -84,39 +94,11 @@ class TestSsm(unittest.TestCase):
         os.chmod(self._msgdir, 0400)
         self._ssm.on_message({'nothing': 'dummy'}, 'Not signed or encrypted.')
         os.chmod(self._msgdir, 0777)
-        
-        
-TEST_CERT = '''-----BEGIN CERTIFICATE-----
-MIICHzCCAYgCCQDmzJkJ04gm+DANBgkqhkiG9w0BAQUFADBUMQswCQYDVQQGEwJ1
-azENMAsGA1UECBMEb3hvbjENMAsGA1UEChMEc3RmYzERMA8GA1UECxMIZXNjaWVu
-Y2UxFDASBgNVBAMTC3dpbGwgcm9nZXJzMB4XDTExMTIwNzEzNTYyMVoXDTEyMTIw
-NjEzNTYyMVowVDELMAkGA1UEBhMCdWsxDTALBgNVBAgTBG94b24xDTALBgNVBAoT
-BHN0ZmMxETAPBgNVBAsTCGVzY2llbmNlMRQwEgYDVQQDEwt3aWxsIHJvZ2VyczCB
-nzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAxfoCCyKgWyaQla0VLuIannHvwR7A
-7zMJk8tze80JOZUPxpFWXhL5F2UJeJtyuHEESp2Sazw4W97nFf25UBVvAbG/TZlz
-1YylVq04XgVgQfpAfaL0RF7vsiEAwfxFVz2jqFYol4sZf1SCwoJXsvl54HIxoyeb
-Y024ykCglELVePECAwEAATANBgkqhkiG9w0BAQUFAAOBgQCVIVzj4MoQ4MUeloer
-9iEDK2V8sRj/BDFQ93cxmiFlsm9k8jENXpGCsOPpO1Q6JjorcXJra+Efb3Prp2Hu
-4e4FwyOLuT7Z/CvS5xW0yBIOeoZdqzLFdMV5GhiVZCJCXG+1Sgs3pK4MFb3ySKII
-nykY+FuXSOT+seB3K9PSk3dj6w==
------END CERTIFICATE-----'''
-            
-TEST_KEY = '''-----BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQDF+gILIqBbJpCVrRUu4hqece/BHsDvMwmTy3N7zQk5lQ/GkVZe
-EvkXZQl4m3K4cQRKnZJrPDhb3ucV/blQFW8Bsb9NmXPVjKVWrTheBWBB+kB9ovRE
-Xu+yIQDB/EVXPaOoViiXixl/VILCgley+XngcjGjJ5tjTbjKQKCUQtV48QIDAQAB
-AoGBAKBzplJWBva5A7d7Js7vizldCD5RaXazu5Bf9MGihFZ52+ZIBmKKJ/1w8sMf
-4VNgrWS33mIw1VCIEGu/TgB8zpB2UEZVCRYCJgVZa4wsJSVmcprvlqOcOl4qUugF
-QkN+f2uG1RT3CbHUV+q0/qnYk5HsU0reaLodaAj9fo8J/OfNAkEA/ntwKVqs/DDk
-bFfedYn3oXTf3o6+lmaPkSVtpr+t3zjR2PEJZCIVkiGMow04m/IXZJSyZWnjCq9X
-kNdXSGWiBwJBAMcoSwNR24jB8zLI2FLRV74rMwdKqoUc0b3J139Xb87UDW2S9Sdz
-N9sJ59TiIut5a49/uiXLiEMQqoGYkORX70cCQBS2Szyyap3kBNNkm3CJmCQF9SqS
-B6UKF+lCWJhXxXkDkODNTWxe8c6A+IdUziSzIYBIMfTbF2WJO+FIBYyY6QUCQDcx
-EWjAHKjPpwgh5OE+pqRK8H9Kz+rHy9BeyVu+7XtSBM6i9VGTep03J4o1iRvcsFQ6
-P2oN95suWTJFB5JgVC0CQDmMynLdOx0KEwgLWXOeeTEqkrZ0l8kIGiaqUhKB45sq
-qY6qrtKDPBP08Alm41ruRf5mqdVwL3OqiPe54JLvLEY=
------END RSA PRIVATE KEY-----'''
-            
+
+TEST_CERT_FILE = '/tmp/test.crt'
+
+TEST_KEY_FILE = '/tmp/test.key'
+ 
 if __name__ == '__main__':
     #import sys;sys.argv = ['', 'Test.test_get_a_broker']
     unittest.main()
