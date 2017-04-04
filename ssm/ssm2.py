89478df34b9ea5234843ee58bf507a6b5aa4fedb
Greg Corbett <greg.corbett@stfc.ac.uk>
2017-04-04 09:19:20 +0100

    Throw exception on init if certificate expired
    - as the system will currently continue to send if the signing cert has
      expired, only for the message to be rejected by the reciever because
      the signer cert has expired

--- ssm/ssm2.py
+++ ssm/ssm2.py
@@ -102,6 +102,12 @@ class Ssm2(stomp.ConnectionListener):
         # check that the cert and key match
         if not crypto.check_cert_key(self._cert, self._key):
             raise Ssm2Exception('Cert and key don\'t match.')
+
+        # check the certificate has not expired
+        if not crypto.verify_cert_date(self._cert):
+            raise Ssm2Exception('Certificate %s has expired, so cannot sign messages.'
+                                % self._cert)
+
         # check the server certificate provided
         if enc_cert is not None:
             log.info('Messages will be encrypted using %s', enc_cert)
