9e5543ad5502a0a07bd92bc9103eea949e1ed97a
Greg Corbett <greg.corbett@stfc.ac.uk>
2018-05-10 14:00:24 +0100

    Add a helper method to get the messages
    - a list of messages/files is not stored in the structure
      because it would be difficult to keep the directory
      and the structure in sync, especially when deleting
      files from the underlying directory, because you cant
      iterate over a stored list of files and delete elements
      in that list in a sensible/easy way.
    - mtime is used because (apparently) there is not way to find the
      original date of file creation due to a limitation
      of the underlying filesystem

--- ssm/message_directory.py
+++ ssm/message_directory.py
@@ -16,6 +16,9 @@ limitations under the License.
 @author: Greg Corbett
 """
 
+import os
+
+
 class MessageDirectory(object):
     """A structure for holding Accounting messages in a directory."""
 
@@ -57,3 +60,43 @@ class MessageDirectory(object):
     def remove(self, name):
         """Remove locked element from the queue."""
         raise NotImplementedError()
+
+    def _get_messages(self, sort_by_mtime=False):
+        """
+        Get the messages stored in this MessageDirectory.
+
+        if sort_by_mtime is set to True, the returned list is guaranteed to be
+        in increasing order of modification time.
+
+        mtime is used because (apparently) there is not way to find the
+        original date of file creation due to a limitation
+        of the underlying filesystem
+        """
+        # Get a list of files under self.directory_path in an arbitrary order.
+        file_name_list = os.listdir(self.directory_path)
+
+        if sort_by_mtime:
+            # Working space to hold the unsorted messages
+            # as file paths and mtimes.
+            unsorted_messages = []
+            # Working space to hold the sorted messages as file names.
+            sorted_messages = []
+
+            # Work out the mtime of each file.
+            for file_name in file_name_list:
+                file_path = os.path.join(self.directory_path, file_name)
+                # Store the file path and the time the file was last modified.
+                unsorted_messages.append((file_name,
+                                          os.path.getmtime(file_path)))
+
+            # Sort the file paths by mtime and then only store the file name.
+            for (file_name, _mtime) in sorted(unsorted_messages,
+                                              key=lambda tup: tup[1]):
+                # Store the sorted file paths in a class element.
+                sorted_messages.append(file_name)
+
+            # Return the sorted list.
+            return sorted_messages
+
+        # If we get here, just return the arbitrarily ordered list.
+        return file_name_list
