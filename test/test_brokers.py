59bc8b32b74b11e27a97094e8244927f69030924
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2019-03-04 17:21:43 +0000

    Mock out external LDAP search calls in tests

--- test/test_brokers.py
+++ test/test_brokers.py
@@ -12,9 +12,11 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from ssm import brokers
-
 import unittest
+
+import mock
+
+from ssm import brokers
 
 
 class Test(unittest.TestCase):
@@ -56,12 +58,15 @@ class Test(unittest.TestCase):
         Requires an internet connection to get information from the BDII.
         Could fail if the BDII is down. This isn't very unit-test-like.
         '''
-        bdii = 'ldap://lcg-bdii.cern.ch:2170'
+        bdii = 'ldap://no-bdii.utopia.ch:2170'
         network = 'PROD'
 
         sbg = brokers.StompBrokerGetter(bdii)
 
-        bs = sbg.get_broker_hosts_and_ports(brokers.STOMP_SERVICE, network)
+        # So that there are no external LDAP calls, mock out the LDAP seach.
+        with mock.patch('ldap.ldapobject.SimpleLDAPObject.search_s',
+                        side_effect=self._mocked_search):
+            bs = sbg.get_broker_hosts_and_ports(brokers.STOMP_SERVICE, network)
 
         if len(bs) < 1:
             self.fail('No brokers found in the BDII.')
@@ -72,6 +77,45 @@ class Test(unittest.TestCase):
 
         if '.' not in host:
             self.fail("Didn't get a hostname from fetch_brokers()")
+
+    def _mocked_search(*args, **kwargs):
+        """Return values to mocked search call based on input."""
+
+        if (
+            '(&(objectClass=GlueService)(GlueServiceType=msg.broker.stomp))'
+        ) in args:
+            return [(
+                'GlueServiceUniqueID=mq.cro-ngi.hr_msg.broker.stomp_3523291347'
+                ',Mds-Vo-name=egee.srce.hr,Mds-Vo-name=local,o=grid',
+                {'GlueServiceUniqueID':
+                    ['mq.cro-ngi.hr_msg.broker.stomp_3523291347'],
+                    'GlueServiceEndpoint': ['stomp://mq.cro-ngi.hr:6163/']}),
+                    (
+                'GlueServiceUniqueID=broker-prod1.argo.grnet.gr_msg.broker.sto'
+                'mp_175215210,Mds-Vo-name=HG-06-EKT,Mds-Vo-name=local,o=grid',
+                {'GlueServiceUniqueID':
+                    ['broker-prod1.argo.grnet.gr_msg.broker.stomp_175215210'],
+                    'GlueServiceEndpoint':
+                    ['stomp://broker-prod1.argo.grnet.gr:6163/']}
+            )]
+        elif (
+            '(&(GlueServiceDataKey=cluster)(GlueChunkKey=GlueServiceUniqueID='
+            'mq.cro-ngi.hr_msg.broker.stomp_3523291347))'
+        ) in args:
+            return [(
+                'GlueServiceDataKey=cluster,GlueServiceUniqueID=mq.cro-ngi.hr_'
+                'msg.broker.stomp_3523291347,Mds-Vo-name=egee.srce.hr,Mds-Vo-n'
+                'ame=local,o=grid', {'GlueServiceDataValue': ['PROD']}
+            )]
+        elif (
+            '(&(GlueServiceDataKey=cluster)(GlueChunkKey=GlueServiceUniqueID='
+            'broker-prod1.argo.grnet.gr_msg.broker.stomp_175215210))'
+        ) in args:
+            return [(
+                'GlueServiceDataKey=cluster,GlueServiceUniqueID=broker-prod1.a'
+                'rgo.grnet.gr_msg.broker.stomp_175215210,Mds-Vo-name=HG-06-EKT'
+                ',Mds-Vo-name=local,o=grid', {'GlueServiceDataValue': ['PROD']}
+            )]
 
 
 if __name__ == '__main__':
