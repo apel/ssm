30004e5ca7d4152b085d8ba49c37d123a495b73e
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2019-03-06 13:13:48 +0000

    Rearrange start_connection to check conn earlier
    Rearrange start_connection to check if connected just after connecting
    so that subscription is not attempted without a connection.

--- ssm/ssm2.py
+++ ssm/ssm2.py
@@ -622,15 +622,6 @@ class Ssm2(stomp.ConnectionListener):
 
         self._conn.start()
         self._conn.connect(wait=False)
-        if self._dest is not None:
-            log.info('Will send messages to: %s', self._dest)
-
-        if self._listen is not None:
-            # Use a static ID for the subscription ID because we only ever have
-            # one subscription within a connection and ID is only considered
-            # to differentiate subscriptions within a connection.
-            self._conn.subscribe(destination=self._listen, id=1, ack='auto')
-            log.info('Subscribing to: %s', self._listen)
 
         i = 0
         while not self.connected:
@@ -640,6 +631,16 @@ class Ssm2(stomp.ConnectionListener):
                 err += 'Check the connection details.'
                 raise Ssm2Exception(err)
             i += 1
+
+        if self._dest is not None:
+            log.info('Will send messages to: %s', self._dest)
+
+        if self._listen is not None:
+            # Use a static ID for the subscription ID because we only ever have
+            # one subscription within a connection and ID is only considered
+            # to differentiate subscriptions within a connection.
+            self._conn.subscribe(destination=self._listen, id=1, ack='auto')
+            log.info('Subscribing to: %s', self._listen)
 
     def close_connection(self):
         '''
