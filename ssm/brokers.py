26e4d923fd88995bda54bc82aca5d7a39c51937a
James Adams <james.adams@stfc.ac.uk>
2019-08-01 11:35:29 +0100

    Remove trailing whitespace from ssm/brokers.py

--- ssm/brokers.py
+++ ssm/brokers.py
@@ -12,10 +12,10 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-   
+
    @author: Will Rogers
 
-Class to interact with a BDII LDAP server to retrieve information about 
+Class to interact with a BDII LDAP server to retrieve information about
 the stomp brokers specified in a network.
 '''
 import ldap
@@ -32,10 +32,10 @@ STOMP_SSL_PREFIX = 'stomp+ssl'
 
 class StompBrokerGetter(object):
     '''
-    Given the URL of a BDII, searches for all the STOMP 
+    Given the URL of a BDII, searches for all the STOMP
     brokers listed that are part of the specified network.
     '''
-    
+
     def __init__(self, bdii_url):
         '''
         Set up the LDAP connection and strings which are re-used.
@@ -48,28 +48,28 @@ class StompBrokerGetter(object):
         self._service_id_key = 'GlueServiceUniqueID'
         self._endpoint_key  = 'GlueServiceEndpoint'
         self._service_data_value_key = 'GlueServiceDataValue'
-        
+
     def get_broker_urls(self, service_type, network):
         '''
-        Gets the list of all the stomp brokers in the BDII, then 
+        Gets the list of all the stomp brokers in the BDII, then
         checks them to see if they are part of the network.  The network
         is supplied as a string.
         Returns a list of URLs.
         '''
         prod_broker_urls = []
-         
+
         broker_details = self._get_broker_details(service_type)
-        
+
         for broker_id, broker_url in broker_details:
             if self._broker_in_network(broker_id, network):
                 prod_broker_urls.append(broker_url)
-            
+
         return prod_broker_urls
-    
+
     def get_broker_hosts_and_ports(self, service_type, network):
         '''
-        Gets the list of all the stomp brokers in the BDII, then 
-        checks them to see if they are part of the network. The network 
+        Gets the list of all the stomp brokers in the BDII, then
+        checks them to see if they are part of the network. The network
         is supplied as a string.
         Returns a list of (host, port) tuples.
         '''
@@ -78,35 +78,35 @@ class StompBrokerGetter(object):
         for url in urls:
             hosts_and_ports.append(parse_stomp_url(url))
         return hosts_and_ports
-        
+
     def _get_broker_details(self, service_type):
         '''
-        Searches the BDII for all STOMP message brokers.  Returns a list of 
+        Searches the BDII for all STOMP message brokers.  Returns a list of
         tuples: (<GlueServiceUniqueID>, <URL>).
         '''
         broker_details = []
-        
-        ldap_filter = '(&(objectClass=GlueService)(GlueServiceType=%s))' % service_type 
+
+        ldap_filter = '(&(objectClass=GlueService)(GlueServiceType=%s))' % service_type
         attrs = [self._service_id_key, self._endpoint_key]
 
-        brokers = self._ldap_conn.search_s(self._base_dn, ldap.SCOPE_SUBTREE, ldap_filter, attrs) 
-        
+        brokers = self._ldap_conn.search_s(self._base_dn, ldap.SCOPE_SUBTREE, ldap_filter, attrs)
+
         for unused_dn, attrs in brokers:
-            details = attrs[self._service_id_key][0], attrs[self._endpoint_key][0] 
+            details = attrs[self._service_id_key][0], attrs[self._endpoint_key][0]
             broker_details.append(details)
-            
+
         return broker_details
-                    
+
     def _broker_in_network(self, broker_id, network):
         '''
-        Given a GlueServiceUniqueID for a message broker, check that it is 
+        Given a GlueServiceUniqueID for a message broker, check that it is
         part of the specified network.
         '''
         ldap_filter = '(&(GlueServiceDataKey=cluster)(GlueChunkKey=GlueServiceUniqueID=%s))' \
             % broker_id
         attrs = [self._service_data_value_key]
         results = self._ldap_conn.search_s(self._base_dn, ldap.SCOPE_SUBTREE, ldap_filter, attrs)
-    
+
         try:
             unused_dn, attrs2 = results[0]
             return network in attrs2[self._service_data_value_key]
@@ -119,16 +119,16 @@ def parse_stomp_url(stomp_url):
     return a tuple containing (stomp.cern.ch, 6262).
     '''
     parts = stomp_url.split(':')
-    
+
     protocols = [STOMP_PREFIX, STOMP_SSL_PREFIX]
     if not parts[0].lower() in protocols:
         raise ValueError("URL %s does not begin 'stomp:'." % stomp_url)
-    
+
     host = parts[1].strip('/')
     port = parts[2].strip('/')
     if not port.isdigit():
         raise ValueError('URL %s does not have an integer as its third part.')
-    
+
     return host, int(port)
 
 
