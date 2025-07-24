bfb354463b649d3d4d8e60f466458a3a899084d4
Sae126V <saitejav2021@gmail.com>
2025-07-24 10:16:59 +0100

    Move *.conf file to usr/lib from etc folder and rename the filename

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ def main():
           scripts=['bin/ssmreceive', 'bin/ssmsend'],
           data_files=[(conf_dir, conf_files),
                       ('/etc/logrotate.d', ['conf/apel-ssm']),
-                      ('/etc/tmpfiles.d', ['scripts/run-ssm.conf']),
+                      ('/usr/lib/tmpfiles.d', ['scripts/apel-ssm.conf']),
                       ('/usr/share/doc/apel-ssm', ['apel-ssm']),
                       # Create empty directories
                       ('/var/log/apel', []),
